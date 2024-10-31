5608752c94a846ddc0a6ce7110efba3e9fe176dd
OliverStoll <oliverstoll.berlin@gmail.com>
2024-10-31 09:51:47 +0100

    Add pretrained to script

--- src/.run_scripts/run.sh
+++ src/.run_scripts/run.sh
@@ -14,6 +14,11 @@ export PYTHONPATH=$PYTHONPATH:$INCLUDE_PATH
 # Activate the virtual environment
 source "$VENV_PATH/bin/activate"
 
+# Print all pip packages installed as one string
+echo "PIP PACKAGES INSTALLED"
+all_packages=$(pip freeze)
+echo $all_packages
+
 # RUN TRAINING WITH SRUN
 python /home/olivers/master-thesis/src/.run_scripts/$SCRIPT_NAME $DATASET $MODELS $PRETRAINED
 
