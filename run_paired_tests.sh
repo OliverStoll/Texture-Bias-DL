f630080ede1c8c945d49d662217a5c3358f8c740
OliverStoll <oliverstoll.berlin@gmail.com>
2024-11-04 11:44:12 +0100

    Fix max-time for slurm

--- run_paired_tests.sh
+++ run_paired_tests.sh
@@ -4,15 +4,8 @@ MODELS=$2
 # define GPU and CPU for srun
 GPU=1
 CPU=8
-hours=99
-# zero pad the hours if less than 2 digits
-if [ ${#hours} -eq 1 ]; then
-    hours=0$hours
-fi
 
-
-TIMESTAMP="$hours:00:00"
-echo "TESTS ($TIMESTAMP) with $GPU GPUs and $CPU CPUs with [ $DATASET | $MODELS ]"
+echo "TESTS with $GPU GPUs and $CPU CPUs with [ $DATASET | $MODELS ]"
 
 # run the run.sh script
-sbatch --gpus=$GPU --cpus-per-task=$CPU --time=$TIMESTAMP --job-name=$DATASET -o logs/slurm/$DATASET.out --wrap="./src/.run_scripts/run.sh test_paired_transforms.py $DATASET $MODELS"
+sbatch --gpus=$GPU --cpus-per-task=$CPU --job-name=$DATASET -o logs/slurm/$DATASET.out --wrap="./src/.run_scripts/run.sh test_paired_transforms.py $DATASET $MODELS"
