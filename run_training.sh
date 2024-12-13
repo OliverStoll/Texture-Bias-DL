c26e89e910f5efaf76fa2f71cc20b55431558b3a
OliverStoll <oliverstoll.berlin@gmail.com>
2024-12-13 15:18:55 +0100

    Plotholes

--- run_training.sh
+++ run_training.sh
@@ -18,5 +18,24 @@ echo "TRAINING with $GPU GPUs and $CPU CPUs with [ $DATASET | $MODELS | $PRETRAI
 
 OUTPUT_LOG="logs/slurm/$DATASET.out"
 
+# if no dataset is provided, iterate over all
+
+
+
+
+if [[ -n "${DATASET}" ]]; then
+  # take the dataset as only element in list
+  echo "Training only on $DATASET"
+  ALL_DATASETS=($DATASET)
+else
+  echo "Training on all datasets"
+  ALL_DATASETS=("imagenet" "rgb-bigearthnet" "bigearthnet" "deepglobe" "caltech" "caltech-ft" "caltech-120")
+
+
+
+
+for dataset in "${ALL_DATASETS[@]}"; do
 # run the run.sh script
-sbatch --gpus=$GPU --cpus-per-task=$CPU --job-name=$DATASET -o $OUTPUT_LOG --wrap="$RUNNER_SCRIPT_PATH $PYTHON_SCRIPT_NAME $DATASET $MODELS $PRETRAINED"
+  echo "Starting training on $dataset"
+  sbatch --gpus=$GPU --cpus-per-task=$CPU --job-name=dataset -o $OUTPUT_LOG --wrap="$RUNNER_SCRIPT_PATH $PYTHON_SCRIPT_NAME $dataset $MODELS $PRETRAINED"
+done
